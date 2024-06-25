273e7cc7de0be96dee2a115d0d7b4df463a313b9
dmlwls12 <gimuijin572@gmail.com>
2024-06-25 12:27:40 +0900

    #4 #17 feat: Initialize inventory management package structure and develop core functionalities

--- ssgnack/src/main/java/com/ssgnack/inboundStock/model/dao/InboundStockMapper.java
+++ ssgnack/src/main/java/com/ssgnack/inboundStock/model/dao/InboundStockMapper.java
@@ -1,4 +1,12 @@
 package com.ssgnack.inboundStock.model.dao;
 
+import com.ssgnack.common.paging.SelectCriteria;
+import com.ssgnack.inboundStock.model.dto.StockDTO;
+
+import java.util.List;
+
 public interface InboundStockMapper {
+    int selectTotalCount();
+
+    List<StockDTO> findAllStock(SelectCriteria selectCriteria);
 }
--- ssgnack/src/main/java/com/ssgnack/inboundStock/model/dto/StockDTO.java
+++ ssgnack/src/main/java/com/ssgnack/inboundStock/model/dto/StockDTO.java
@@ -1,4 +1,16 @@
 package com.ssgnack.inboundStock.model.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class StockDTO {
+
+    private int stock_id;
+    private int stock_amt;
+    private int product_id;
+    private int warehouse_id;
 }
